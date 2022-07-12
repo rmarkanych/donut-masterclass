b36b2cc5e5ae0b0d66fae42f7a4c2103b6cc29c3
ihumentsev <a.ihumentsev@gmail.com>
2022-07-12 02:21:54 +0200

    add-form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import "./js/read-more";
+import "./js/carusel";
---
+++ src/js/carusel.js
@@ -0,0 +1,53 @@
+//  import $ from 'jquery';
+$(".slider-conteiner").slick({
+ 
+  // normal options...
+    infinite: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    mobileFirst:true,
+
+ 
+  // the magic
+  responsive: [{
+ 
+      breakpoint: 1280,
+      settings: {
+      slidesToShow: 3,
+      infinite: true,
+        
+      }
+ 
+    }, {
+ 
+      breakpoint: 768,
+      settings: {
+      slidesToShow: 3,
+      infinite: true,
+        
+      }
+ 
+      },{
+ 
+      breakpoint: 480,
+      settings: {
+      slidesToShow: 1,
+      infinite: true,
+      
+      }
+ 
+    },{
+ 
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+ 
+    }]
+});
+
+$('.reviews_slider').on(
+    'beforeChange', (e, slick, currSlide, nextSlide) => {
+        if (currSlide === nextSlide) { return };
+
+
+    })
