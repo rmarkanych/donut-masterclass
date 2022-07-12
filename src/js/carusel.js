5962489b8534eb302af6d5329ab79d3bfb225f07
ihumentsev <a.ihumentsev@gmail.com>
2022-07-12 14:59:19 +0200

    add-slider-fitch

--- src/js/carusel.js
+++ src/js/carusel.js
@@ -1,51 +1,31 @@
 //  import $ from 'jquery';
-$(".reviews__slider").slick({
-    asNavFor: '.reviews__slider-nav',
+$('.reviews__slider').slick({
+  asNavFor: '.reviews__slider-nav',
   // normal options...
-    infinite: true,
-    arrows: false,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    mobileFirst: true,
-    
+  infinite: true,
+  arrows: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  mobileFirst: true,
+  variableWidth: true,
+  centerMode: true,
+  centerPadding: '0px',
 
- 
   // the magic
-  responsive: [{
- 
-      breakpoint: 1280,
+  responsive: [
+    {
+      breakpoint: 767,
       settings: {
-      slidesToShow: 3,
-      infinite: true,
-        
-      }
- 
-    }, {
- 
-      breakpoint: 768,
-      settings: {
-      slidesToShow: 3,
-      infinite: true,
-        
-      }
- 
-      },{
- 
-      breakpoint: 480,
-      settings: {
-      slidesToShow: 1,
-      infinite: true,
-      
-      }
- 
-    },{
- 
-      breakpoint: 300,
-      settings: "unslick" // destroys slick
- 
-    }]
+        slidesToShow: 3,
+      },
+    },
+  ],
 });
 
 $('.reviews__slider-nav').slick({
-   asNavFor: '.reviews__slider'
-})
+  asNavFor: '.reviews__slider',
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+});
