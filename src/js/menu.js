cf7e9554d40843119fa587ed6fd9081236dc6fa3
Roman <roman.markanych@gmail.com>
2022-07-12 21:01:34 +0300

    add changes

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,9 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
-const body = document.querySelector("body");
+  const body = document.querySelector("body");
+  const mobileLinkRef = document.querySelector("#menu");
+  
   menuBtnRef.addEventListener("click", () => {
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
@@ -13,5 +15,17 @@ const body = document.querySelector("body");
     body.classList.toggle("no-scroll");
     
   });
+ 
+
+mobileLinkRef.addEventListener("click", () => {
+        const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
+        mobileMenuRef.classList.toggle("is-open");
+        body.classList.toggle("no-scroll");
+      
+});
   
 })();
+
+
