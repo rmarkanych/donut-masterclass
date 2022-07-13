7f070a39619dedf286b86f586034f372decd594b
Roman <roman.markanych@gmail.com>
2022-07-13 12:04:11 +0300

    add main and fix bugs with scroll-scrip

--- src/js/menu-scroll.js
+++ src/js/menu-scroll.js
@@ -1,15 +1,18 @@
-const links = document.querySelectorAll(".header-nav ul  a");
-for (const link of links) {
-  link.addEventListener("click", clickHandler);
-}
+const menuLinks = document.querySelectorAll('.menu-nav__link');
+const headerLinks = document.querySelectorAll('.header-nav__link');
+const headerTxtLinks = document.querySelectorAll('.header-btn');
+
+menuLinks.forEach(link => link.addEventListener('click', clickHandler));
+headerLinks.forEach(link => link.addEventListener('click', clickHandler));
+headerTxtLinks.forEach(link => link.addEventListener('click', clickHandler));
 
 function clickHandler(e) {
   e.preventDefault();
-  const href = this.getAttribute("href");
+  const href = this.getAttribute('href');
   const offsetTop = document.querySelector(href).offsetTop;
 
   scroll({
     top: offsetTop,
-    behavior: "smooth"
+    behavior: 'smooth',
   });
 }
